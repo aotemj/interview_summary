270f1efb60956f0fbc933d57c869f2d9cd406d49
zhaoxinlei <991807272@qq.com>
2021-10-07 15:48:32 +0800

    promise 并发控制

---
+++ promise-limit.js
@@ -0,0 +1,140 @@
+/**
+ * 实现并发调用，最大并发数
+ * @param urls 需要调用的url 信息
+ * @param handler 处理函数
+ * @param limit 并发数
+ */
+function limitLoad(urls, handler, limit) {
+  const copyUrls = Array.from(urls);
+  let promises = copyUrls.splice(0, limit).map((item, index) => {
+    return handler(item)
+  })
+
+  let p = Promise.race(promises);
+  let length = copyUrls.length
+  for (let i = 0; i < length; i++) {
+    p = p.then(res => {
+      promises[res] = handler(copyUrls[i]).then(() => {
+        return res
+      })
+
+      return Promise.race(promises)
+    })
+  }
+}
+
+const urls = [
+  {
+    info: 'xxx1',
+    time: 3000,
+    index: 0
+  },
+  {
+    info: 'xxx2',
+    time: 2000,
+    index: 1
+  }, {
+    info: 'xxx3',
+    time: 3000,
+    index: 2
+  },
+  {
+    info: 'xxx4',
+    time: 2000,
+    index: 3
+  }, {
+    info: 'xxx5',
+    time: 3000,
+    index: 4
+  },
+  {
+    info: 'xxx6',
+    time: 2000,
+    index: 5
+  }, {
+    info: 'xxx7',
+    time: 3000,
+    index: 6
+  },
+  {
+    info: 'xxx8',
+    time: 2000,
+    index: 7
+  }, {
+    info: 'xxx9',
+    time: 3000,
+    index: 8
+  },
+  {
+    info: 'xxx10',
+    time: 2000,
+    index: 9
+  }, {
+    info: 'xxx11',
+    time: 3000,
+    index: 10
+  },
+  {
+    info: 'xxx12',
+    time: 2000,
+    index: 11
+  }, {
+    info: 'xxx13',
+    time: 3000,
+    index: 12
+  },
+  {
+    info: 'xxx14',
+    time: 2000,
+    index: 13
+  }, {
+    info: 'xxx15',
+    time: 3000,
+    index: 14
+  },
+  {
+    info: 'xxx16',
+    time: 2000,
+    index: 15
+  }, {
+    info: 'xxx17',
+    time: 3000,
+    index: 16
+  },
+  {
+    info: 'xxx18',
+    time: 2000,
+    index: 17
+  }, {
+    info: 'xxx19',
+    time: 3000,
+    index: 18
+  },
+  {
+    info: 'xxx20',
+    time: 2000,
+    index: 19
+  }, {
+    info: 'xxx21',
+    time: 3000,
+    index: 20
+  },
+  {
+    info: 'xxx22',
+    time: 2000,
+    index: 21
+  },
+]
+
+function loadImg(url) {
+  if (!url) return
+  return new Promise((resolve, reject) => {
+    console.log("---" + url.info + " start")
+    setTimeout(() => {
+      console.log(url.info + " ok!!!")
+      resolve(url.index)
+    }, url.time)
+  })
+}
+
+limitLoad(urls, loadImg, 3)
