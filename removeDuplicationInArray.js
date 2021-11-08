6e91848e1c72fbb51020ef1c295f24cb055a4094
ceui <991807272@qq.com>
2021-11-09 06:01:54 +0800

    remove duplication in array method1: use regExp

--- removeDuplicationInArray.js
+++ removeDuplicationInArray.js
@@ -2,7 +2,7 @@
  * remove duplication in array
  */
 
-let arr = [1, 1, 1, 2, 3, 3, 4, 45, 5, 4, 2, 5, 5, 5, 3, 2]
+let arr = [1, 1, 1, 2, 3, 3, 4, 45, 5, 4, 2, 5, 5, 5, 3, 2, 18, 128, 128]
 // 方法1： new Set
 {
   let newArr = [...new Set(arr)]
@@ -18,5 +18,16 @@ let arr = [1, 1, 1, 2, 3, 3, 4, 45, 5, 4, 2, 5, 5, 5, 3, 2]
       newArr.push(item)
     }
   }
+  console.log(newArr);
+}
+// 方法3： 相邻项的处理
+{
+  let newArr = [], arrStr = arr.sort((a, b) => a - b).join('#') + '#'
+  let regExp = /(\d+#)\1*/g
+  console.log(arrStr);
+  arrStr.replace(regExp, (val, group1) => {
+    console.log(val, group1);
+    newArr.push(parseInt(group1))
+  })
   console.log(newArr);
 }
