f721e92411286de2fa0b3bc1475d39acb163f0bc
ceui <991807272@qq.com>
2021-11-09 05:46:52 +0800

    remove duplication in array method1: use hash

--- removeDuplicationInArray.js
+++ removeDuplicationInArray.js
@@ -2,11 +2,21 @@
  * remove duplication in array
  */
 
+let arr = [1, 1, 1, 2, 3, 3, 4, 45, 5, 4, 2, 5, 5, 5, 3, 2]
 // 方法1： new Set
 {
-  let arr = [1, 1, 1, 2, 3, 3, 4, 45, 5, 4, 2, 5, 5, 5, 3, 2]
   let newArr = [...new Set(arr)]
   console.log(newArr);
 }
 
-//
+// 方法2： hash
+{
+  let map = new Map(), newArr = []
+  for (const item of arr) {
+    if (!map.has(item)) {
+      map.set(item, 1)
+      newArr.push(item)
+    }
+  }
+  console.log(newArr);
+}
