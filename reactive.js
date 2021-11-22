3682629383f7d0988ba674db8cea971c72b78283
ceui <991807272@qq.com>
2021-11-22 22:30:23 +0800

    manual reactive

---
+++ reactive.js
@@ -0,0 +1,50 @@
+const {toType} = require('./toType/toType')
+
+const render = (key, val) => {
+  console.log(`SET key=${key} val=${val}`)
+}
+
+
+const data = {
+  a: 1,
+  b: 2,
+  c: {
+    c1: {
+      af: 999,
+    },
+    c2: 4
+  }
+}
+const defineReactive = (obj, key, val) => {
+  Object.defineProperty(obj, key, {
+    get() {
+      return val;
+    },
+    set(newVal) {
+      if (newVal === val) return;
+      val = newVal;
+      render(key, val)
+    }
+  })
+}
+
+function reactive(obj) {
+  if (toType(obj) === 'object') {
+    for (const key in obj) {
+      const val = obj[key]
+      if (toType(val) === 'object') {
+        reactive(val)
+      } else {
+        defineReactive(obj, key, val)
+      }
+    }
+  }
+}
+
+reactive(data)
+
+data.a = 5 // SET key = a  val = 5;
+data.b = 7 // SET key = b val = 7;
+data.c.c2 = 4 //
+data.c.c1.af = 121 // SET key = af v al = 121
+// console.table(data);
