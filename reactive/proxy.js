0de197d7efc16fd785d173259379f139e436fce0
ceui <991807272@qq.com>
2021-11-25 05:44:59 +0800

    manual reactive for proxy

--- reactive/proxy.js
+++ reactive/proxy.js
@@ -1,34 +1,39 @@
 let observableStore = new Map()
 
-
 function makeObservable(target) {
-  let handlerName = Symbol('handler');
+  let handlerName = Symbol('handler')
   observableStore.set(handlerName, [])
   target.observe = function (handler) {
     observableStore.get(handlerName).push(handler)
   }
+
   const handler = {
     get(target, property, receiver) {
-      console.log(receiver);
-      observableStore.get(handlerName).forEach(item => {
-        item('GET', property, target[property])
-      })
-      return target[property]
+      let success = Reflect.get(target, property, receiver)
+      if (success) {
+        observableStore.get(handlerName).forEach(item => {
+          item('GET', property, target[property])
+        })
+        return target[property]
+      }
     },
     set(target, property, value, receiver) {
-      target[property] = value
-      observableStore.get(handlerName).forEach(item => {
-        item('SET', property, value)
-      })
+      let success = Reflect.set(target, property, value, receiver);
+      if (success) {
+        observableStore.get(handlerName).forEach(item => {
+          item('SET', property, value)
+        })
+      }
     },
-    deleteProperty(target, propertyKey) {
-      delete target[propertyKey]
-      observableStore.get(handlerName).forEach(item => {
-        item('DELETE', propertyKey)
-      })
+    deleteProperty(target, p) {
+      let success = Reflect.deleteProperty(target, p)
+      if (success) {
+        observableStore.get(handlerName).forEach(item => {
+          item('DELETE', p, target[p])
+        })
+      }
     }
   }
-
   let p = new Proxy(target, handler)
   return p
 }
